472b7cdc3febdbb7b7f8b8d3fb77ca6855bf7e2a
Yttruire <teozhihao99@gmail.com>
2021-10-10 23:15:53 +0800

    Update TypicalStudents and add TypicalGroups

---
+++ src/test/java/seedu/address/testutil/TypicalGroups.java
@@ -0,0 +1,11 @@
+package seedu.address.testutil;
+
+import seedu.address.model.group.Group;
+
+/**
+ * A utility class containing a list of {@code Group} objects to be used in tests.
+ */
+public class TypicalGroups {
+
+    public static final Group TYPICAL_GROUP = new GroupBuilder().withGroupName("CS2103T").withDescription("hi").build();
+}
--- src/test/java/seedu/address/testutil/TypicalStudents.java
+++ src/test/java/seedu/address/testutil/TypicalStudents.java
@@ -10,25 +10,20 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TELEGRAM_HANDLE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TELEGRAM_HANDLE_BOB;
+import static seedu.address.testutil.TypicalGroups.TYPICAL_GROUP;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import seedu.address.model.AddressBook;
-import seedu.address.model.group.Description;
 import seedu.address.model.group.Group;
-import seedu.address.model.group.GroupName;
 import seedu.address.model.student.Student;
 
 /**
  * A utility class containing a list of {@code Student} objects to be used in tests.
  */
 public class TypicalStudents {
-
-    public static final GroupName TYPICAL_GROUP_GROUPNAME = new GroupName("CS2103T");
-    public static final Description TYPICAL_GROUP_DESCRIPTION = new Description("hi");
-    public static final Group TYPICAL_GROUP = new Group(TYPICAL_GROUP_GROUPNAME, TYPICAL_GROUP_DESCRIPTION);
 
     public static final Student ALICE = new StudentBuilder().withName("Alice Pauline").withEmail("alice@example.com")
             .withGroup(TYPICAL_GROUP).withTelegramHandle("@alice_pauline").build();
@@ -54,12 +49,12 @@ public class TypicalStudents {
     // Manually added - Student's details found in {@code CommandTestUtil}
     public static final Student AMY = new StudentBuilder().withName(VALID_NAME_AMY)
             .withTelegramHandle(VALID_TELEGRAM_HANDLE_AMY).withEmail(VALID_EMAIL_AMY)
-            .withGroup(VALID_GROUP_NAME_AMY, VALID_DESC_AMY).build();
+            .withGroup(new GroupBuilder().withGroupName(VALID_GROUP_NAME_AMY).withDescription(VALID_DESC_AMY).build())
+            .build();
     public static final Student BOB = new StudentBuilder().withName(VALID_NAME_BOB)
             .withTelegramHandle(VALID_TELEGRAM_HANDLE_BOB).withEmail(VALID_EMAIL_BOB)
-            .withGroup(VALID_GROUP_NAME_BOB, VALID_DESC_BOB).build();
-
-    //TODO GroupBuilder in the future?
+            .withGroup(new GroupBuilder().withGroupName(VALID_GROUP_NAME_BOB).withDescription(VALID_DESC_BOB).build())
+            .build();
 
     public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
 
@@ -86,6 +81,6 @@ public class TypicalStudents {
     }
 
     public static List<Group> getTypicalGroups() {
-        return new ArrayList<>(List.of(GROUPCS2103T));
+        return new ArrayList<>(List.of(TYPICAL_GROUP));
     }
 }
